e1a775362fd6fb639786326f967de732715c32b9
Gregory <gregory.nicholas.mayer@proton.me>
2023-08-29 02:00:51 -0300

    add confirmation

--- criando-seu-primeiro-projeto-com-ruby.rb
+++ criando-seu-primeiro-projeto-com-ruby.rb
@@ -11,10 +11,14 @@ def execute
     puts "Insira sua IDADE"
     age = gets.chomp.to_i
 
-    if (/\d|\W/.match(name+last_name) == nil) && (age > 0) && (!name.empty?) && (!last_name.empty?)
-      puts "Salvando dados..."
-      puts "Cadastro realizado\nNOME: #{name}\nSOBRENOME: #{last_name}\nIDADE: #{age}"
-      validation = 1
+    if (/\d|\W/.match(name+last_name) == nil) && (age > 0) && (!name.empty?) && (!last_name.empty?) && (name != last_name)
+      puts "Confirme os dados:\nNOME: #{name}\nSOBRENOME: #{last_name}\nIDADE: #{age}"
+      puts "Digite 1 para confirmar e 2 para preencher novamente."
+      confirm = gets.chomp.to_i
+      if confirm == 1
+        puts "Salvando dados..."
+        validation = 1
+      end
     else
       puts "Preenchimento Incorreto. Por favor, tente novamente."
     end
